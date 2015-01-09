bf26d3213d60af9d4bf13f98c2e645cac76f2560
Sarah Cheng <s_cheng@alum.mit.edu>
2015-01-08 23:05:20 -0500

    revert controller-specific style changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-%w( welcome streams public users ).each do |controller|
-  Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
-end
