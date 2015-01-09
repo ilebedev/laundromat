9a1c68c3a66467bca36ba25e3610cd79bab491b5
Sarah Cheng <s_cheng@alum.mit.edu>
2015-01-09 00:32:46 -0500

    fix streams table style, add video-js

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Laundromat
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+	config.assets.paths << "#{Rails.root}/app/assets/videos"
   end
 end
