5e14669870288e515bfcd7c65254661de3578ffe
Ilia Lebedev <ilebedev@mit.edu>
2015-07-29 15:08:23 -0400

    render links correctly in requests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  require 'uri'
+
+  def uri?(string)
+    uri = URI.parse(string)
+    %w( http https ).include?(uri.scheme)
+  rescue URI::BadURIError
+    false
+  rescue URI::InvalidURIError
+    false
+  end
 end
